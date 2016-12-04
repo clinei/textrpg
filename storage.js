90b96dc41f398827f6b28310bc1ad92b749a23e8
Tanel Tagaväli <tanel58@hotmail.com>
2016-12-04 22:59:36 +0200

    fixed initialization

--- storage.js
+++ storage.js
@@ -1,15 +1,20 @@
-var storage = {
+var storageFuncs = {
 	init: function() {
 		localStorage.version = "0.0.1";
 		localStorage.scene = "StartCave";
 	},
 };
 
-storage = new Proxy(storage, {
+storage = new Proxy(storageFuncs, {
 	set(prop, val) {
 		localStorage.setItem(prop, val);
 	},
 	get(target, prop) {
-		return localStorage[prop];
+		var res = storageFuncs[prop];
+		if (res) {
+			return res;
+		} else {
+			return localStorage[prop];
+		}
 	}
 });
